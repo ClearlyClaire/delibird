cc6092166693f202bf821d036e45f1ba18c077da
Thibaut Girka <thib@sitedethib.com>
2018-02-09 15:37:23 +0100

    Rework how commands work, allow resolving without fqdn and with mentions

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from data import MEDIA, MSGS, REWARDS
 
 API_BASE = 'https://social.sitedethib.com'
 
-MENTION_RE = re.compile(r'[A-Za-z0-9]+@[a-z0-9\.\-]+[A-Za-z0-9]+')
+COMMAND_RE = re.compile(r'(va voir|go see)\s*(.+)')
+LINK_RE    = re.compile(r'<a href="([^"]+)"')
+MENTION_RE = re.compile(r'([A-Za-z0-9]+)(@[a-z0-9\.\-]+[A-Za-z0-9]+)?')
 
 STATE_OWNED, STATE_IDLE, STATE_DELIVERY = range(3)
 MAX_OWNED = datetime.timedelta(hours=8)
@@ -29,12 +31,14 @@ class Delibird(StreamListener):
     print('Delibird started!')
     self.load()
 
+
   def save(self):
     state = {'like_count': self.like_count,
              'visited_users': list(self.visited_users),
              'reward_level': -1}
     with open('state.json', 'w') as file:
       json.dump(state, file)
+
 
   def load(self):
     try:
@@ -46,6 +50,7 @@ class Delibird(StreamListener):
     except FileNotFoundError:
       pass
 
+
   def handle_rewards(self):
     level = -1
     for i, reward in enumerate(REWARDS):
@@ -56,10 +61,12 @@ class Delibird(StreamListener):
       self.send_toot(REWARDS[level]['msg_id'], nb_likes=self.like_count, nb_users=len(self.visited_users))
       self.save()
 
+
   def upload_media(self, name):
     media = self.mastodon.media_post(MEDIA[name]['file'], description='Source: %s' % MEDIA[name]['source'])
     print('Uploaded %s!' % name)
     return media
+
 
   def send_toot(self, msg_id, in_reply_to_id=None, **kwargs):
     print('Sending a toot… id: %s' % msg_id)
@@ -67,23 +74,57 @@ class Delibird(StreamListener):
     media = [self.upload_media(name) for name in msg['media']] if 'media' in msg else None
     self.mastodon.status_post(msg['text'].format(**kwargs), media_ids=media, in_reply_to_id=in_reply_to_id, visibility=msg.get('privacy', ''))
 
+
   def handle_mention(self, status):
     print('Got a mention!')
+    # Only reply to valid commands
+    match = COMMAND_RE.search(status.content)
+    if not match:
+      return
+    # Do not reply if multiple people are mentionned
+    if len(status.mentions) > 2:
+      return
+
+    # Now, we will always reply with something!
+    text_with_user = match.group(2)
+
     if self.state == STATE_DELIVERY:
       return self.send_toot('ERROR_DELIVERY', status, sender_acct=status.account.acct)
     if self.state == STATE_OWNED and self.owner and self.owner.id != status.account.id:
       return self.send_toot('ERROR_OWNED', status, sender_acct=status.account.acct)
-    match = MENTION_RE.search(status.content)
-    if not match:
+    receiver_acct = None
+
+    # First, see if we have a handle we can resolve, without the leading '@'
+    match = MENTION_RE.match(text_with_user)
+    if match:
+      if match.group(2):
+        # If it's a full handle, use it
+        receiver_acct = match.group(0)
+      else:
+        # If it's *not* a full handle, append the user's domain
+        receiver_acct = '@'.join([match.group(1)] + status.account.acct.split('@')[1:])
+    else:
+      # Maybe it's a link to their profile, or a mention. Switch to link handling.
+      match = LINK_RE.search(text_with_user)
+      if match:
+        url = match.group(1)
+        matches = self.mastodon.search(url, resolve=True).accounts
+        if matches:
+          receiver_acct = matches[0].acct
+
+    if not receiver_acct:
       return self.send_toot('ERROR_INVALID_FORMAT', status, sender_acct=status.account.acct)
-    matches = self.mastodon.account_search(match.group(0))
+
+    matches = self.mastodon.account_search(receiver_acct)
     if not matches:
-      return self.send_toot('ERROR_UNKNOWN_ACCOUNT', status, sender_acct=status.account.acct, acct=match.group(0))
+      return self.send_toot('ERROR_UNKNOWN_ACCOUNT', status, sender_acct=status.account.acct, acct=receiver_acct)
+
     self.state = STATE_DELIVERY
     self.owner = status.account
     self.last_owned = datetime.datetime.now()
     self.target = matches[0]
     self.send_toot('DELIVERY_START', status, sender_acct=status.account.acct, acct=self.target.acct)
+
 
   def deliver(self):
     self.send_toot('DELIVERED', sender_acct=self.owner.acct, receiver_acct=self.target.acct)
@@ -93,9 +134,11 @@ class Delibird(StreamListener):
     self.visited_users.add(self.owner.id)
     self.save()
 
+
   def go_idle(self):
     self.state = STATE_IDLE
     self.send_toot('IDLE')
+
 
   def handle_heartbeat(self):
     if self.state == STATE_DELIVERY and self.target is not None and random.random() > 0.85:
@@ -105,6 +148,7 @@ class Delibird(StreamListener):
     elif self.state == STATE_OWNED and datetime.datetime.now() - self.last_owned > MAX_OWNED:
       self.go_idle()
 
+
   def on_notification(self, notification):
     if notification.type == 'mention':
       self.handle_mention(notification.status)
@@ -113,12 +157,15 @@ class Delibird(StreamListener):
       self.save()
 
 
+
 def register(args):
   Mastodon.create_app('Delibird', api_base_url=args.api_base, to_file='secrets/clientcred.secret')
+
 
 def login(args):
   mastodon = Mastodon(client_id = 'secrets/clientcred.secret', api_base_url=args.api_base)
   mastodon.log_in(args.user_mail, getpass(), to_file='secrets/usercred.secret')
+
 
 def run(args):
   mastodon = Mastodon(client_id = 'secrets/clientcred.secret',
@@ -128,11 +175,13 @@ def run(args):
   print('Starting streaming!')
   mastodon.stream_user(delibird)
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('command', type=str, choices=['register', 'login', 'run'])
 parser.add_argument('-a', '--api-base', type=str, default=API_BASE)
 parser.add_argument('-u', '--user-mail', type=str)
 args = parser.parse_args()
+
 
 if args.command == 'register':
   register(args)
